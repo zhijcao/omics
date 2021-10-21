f214695f0d2342540213c4612269158a4c01d5db
zhijcao <40358755+zhijcao@users.noreply.github.com>
2021-10-21 15:15:23 -0500

    Create copy_paste_table.R

---
+++ copy_paste_table.R
@@ -0,0 +1,17 @@
+# Paste data into R
+paste_table <- function() {
+  f <- file(description = 'clipboard', open = 'r')
+  df <- read.table(f, sep = '\t', header = TRUE)
+  close(f)
+  return(df)
+}
+
+# Copy data out of R
+copy_table <- function(obj, size = 4096) {
+  clip <- paste('clipboard-', size, sep = '')
+  f <- file(description = clip, open = 'w')
+  write.table(obj, f, row.names = FALSE, sep = '\t')
+  close(f)  
+}
+
+
